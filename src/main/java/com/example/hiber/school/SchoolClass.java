0140a20deb3d66f71ba6541845abb34dd5d31063
Sky_Taill <vorobeichik2015@yandex.ru>
2023-05-05 01:50:53 +0300

    first commit

---
+++ src/main/java/com/example/hiber/school/SchoolClass.java
@@ -0,0 +1,9 @@
+package com.example.hiber.school;
+
+import java.util.List;
+
+public class SchoolClass {
+    private int id;
+    private String name;
+    private List<Student> studentList;
+}
---
+++ src/main/java/com/example/hiber/school/Student.java
@@ -0,0 +1,42 @@
+package com.example.hiber.school;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Student {
+    private int id;
+    private String name;
+    private String surname;
+
+
+    public Student( String name, String surname) {
+
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+
+}
